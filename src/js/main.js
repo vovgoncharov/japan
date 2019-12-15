671233e90a4237e5b28537b3cd62185cbf995b76
vovgoncharov <deykdog@gmail.com>
2019-12-15 15:03:09 +0300

    animate

--- src/js/main.js
+++ src/js/main.js
@@ -4,5 +4,59 @@ $(document).ready(function () {
     });
     $('.header__nav-close').on('click', function () {
         $('.header__nav').removeClass('header__nav_active');
-    })
+    });
+    $(window).scroll(function () {
+        $(".header").each(function () {
+            var elPos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (elPos < topOfWindow + 100) {
+                $(this).addClass("fadeInLeft");
+            }
+        });
+    });
+    $(window).scroll(function () {
+        $(".intro__global").each(function () {
+            var elPos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (elPos < topOfWindow + 100) {
+                $(this).addClass("fadeInLeft");
+            }
+        });
+    });
+    $(window).scroll(function () {
+        $(".city__caption").each(function () {
+            var elPos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (elPos < topOfWindow + 400) {
+                $(this).addClass("rotateInDownLeft");
+            }
+        });
+    });
+    $(window).scroll(function () {
+        $(".city__cards").each(function () {
+            var elPos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (elPos < topOfWindow + 400) {
+                $(this).addClass("zoomInUp");
+            }
+        });
+    });
+    $(window).scroll(function () {
+        $(".one").each(function () {
+            var elPos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (elPos < topOfWindow + 400) {
+                $(this).addClass("fadeInLeft");
+            }
+        });
+    });
+    $(window).scroll(function () {
+        $(".two").each(function () {
+            var elPos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (elPos < topOfWindow + 400) {
+                $(this).addClass("fadeInRight");
+            }
+        });
+    });
 });
